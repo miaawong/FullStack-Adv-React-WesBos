d31b9834dc7a4ac7088538f6f076981971fee209
Mia Wong <miaw629@gmail.com>
2019-08-15 17:39:38 -0400

    created new type of data called item, added to prisma server, retrieved our new prisma.graphql file which contains our queries and mutations. we then go to our own schema.graphql (where react interacts with db), and we create functions like createItems, which we then write mutations and query to maniplate our data

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,21 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        //checked if they are logged in
+        //ctx.db = our database
+        // belows turns a promise, await the creation of item
+        const item = await ctx.db.mutation.createItem(
+            {
+                data: {
+                    ...args
+                }
+            },
+            // info needs to be passed as a 2nd argument because it is the query
+            // ctx.db.mutations needs to take the query from frontend and pass it to
+            // our backend and info will specify what data gets return from the db when we create it
+            info
+        );
+        return item;
+    }
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+// if the query exactly the same on yoga as prisma, you can forward query from yoga to prisma
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+    items: forwardTo("db")
+    // async items(parent, args, ctx, info) {
+    //     // getting all items
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+};
 
 module.exports = Query;
