c6a3fb1bf4eccc19e4ae2099041c73ad8d04781f
Mia Wong <miaw629@gmail.com>
2019-08-18 16:46:09 -0400

    querying data from prisma and displaying it to items page with render props

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+import Item from "./Item";
+
+// queries
+const ALL_ITEMS_QUERY = gql`
+    query ALL_ITEMS_QUERY {
+        # all fields we want to return
+        items {
+            id
+            title
+            price
+            description
+            image
+            largeImage
+        }
+    }
+`;
+
+const Center = styled.div`
+    text-align: center;
+`;
+const ItemsList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+`;
+
+export default class Items extends Component {
+    render() {
+        return (
+            <Center>
+                <p>items</p>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {/* the function  */}
+                    {({ data, error, loading }) => {
+                        if (loading) return <p>loading...</p>;
+                        if (error) return <p>error: {error.message} </p>;
+                        return (
+                            <ItemsList>
+                                {data.items.map(item => (
+                                    <Item item={item} key={item.id}>
+                                        {item.title}
+                                    </Item>
+                                ))}
+                            </ItemsList>
+                        );
+                    }}
+                </Query>
+            </Center>
+        );
+    }
+}
