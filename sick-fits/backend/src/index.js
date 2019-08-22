606ff657251e9be6ef56fd290dfb6443493643ae
Mia Wong <miaw629@gmail.com>
2019-08-22 16:17:22 -0400

    page for signup, and component for signup.

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
@@ -7,7 +8,19 @@ const server = createServer();
 
 // access all the cookies in an object
 server.express.use(cookieParser());
+
 // TODO use express middle to populate current user
+
+//decode the jwt so we can get the user id on each request
+server.express.use((req, res, next) => {
+    const { token } = req.cookies;
+    if (token) {
+        const { userId } = jwt.verify(token, process.env.APP_SECRET);
+        // put the userid onto the request for future request
+        req.userId = userId;
+    }
+    next();
+});
 
 server.start(
     {
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,7 +4,14 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
     items: forwardTo("db"),
     item: forwardTo("db"),
-    itemsConnection: forwardTo("db")
+    itemsConnection: forwardTo("db"),
+    me(parent, args, ctx, info) {
+        //check if there is a current userid
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user({ where: { id: ctx.request.userId } }, info);
+    }
 };
 
 module.exports = Query;
